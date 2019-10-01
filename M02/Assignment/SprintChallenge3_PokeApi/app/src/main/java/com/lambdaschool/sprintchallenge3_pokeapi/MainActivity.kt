67bfd8e4fc694d1a842fc61bb760cefc1678728d
msmaitran <itsmaisn@gmail.com>
2019-10-01 15:46:03 -0700

    Refactor code in MainActivity to use Retrofit.

--- M02/Assignment/SprintChallenge3_PokeApi/app/src/main/java/com/lambdaschool/sprintchallenge3_pokeapi/MainActivity.kt
+++ M02/Assignment/SprintChallenge3_PokeApi/app/src/main/java/com/lambdaschool/sprintchallenge3_pokeapi/MainActivity.kt
@@ -7,9 +7,60 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Callback<JSONPokemon> {
+
+    companion object {
+        val POKEMON_INTENT_KEY = "pokemon"
+    }
+
+    val pokemonList = mutableListOf<PokemonData>()
+    lateinit var pokemonDao: PokemonDao
+
+    override fun onFailure(call: Call<JSONPokemon>, t: Throwable) {
+        t.printStackTrace()
+        val response = "Failure; ${t.printStackTrace()}"
+        Toast.makeText(this@MainActivity, response, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResponse(call: Call<JSONPokemon>, response: Response<JSONPokemon>) {
+        if (response.isSuccessful) {
+            val jsonPokemon = response.body() as JSONPokemon
+            val abilities = mutableListOf<String>()
+            jsonPokemon.abilities.forEach {
+                abilities.add(it.ability.name)
+            }
+
+            val types = mutableListOf<String>()
+            jsonPokemon.types.forEach {
+                types.add(it.type.name)
+            }
+
+            val pokemonData = PokemonData(
+                    jsonPokemon.name,
+                    jsonPokemon.sprites.front_default,
+                    jsonPokemon.id,
+                    abilities,
+                    types
+            )
+
+            pokemonList.add(pokemonData)
+            rv_list.adapter!!.notifyDataSetChanged()
+
+            val intent = Intent(this, PokemonDetailsActivity::class.java)
+            intent.putExtra(POKEMON_INTENT_KEY, pokemonData)
+            startActivity(intent)
+
+        } else {
+            val response = "Response not successful; ${response.errorBody().toString()}"
+            Toast.makeText(this@MainActivity, response, Toast.LENGTH_SHORT).show()
+        }
+    }
 
     internal var context: Context? = null
 
