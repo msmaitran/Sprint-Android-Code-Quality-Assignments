a5660b7734df2a64ec77ee955b316d670ec2084f
msmaitran <itsmaisn@gmail.com>
2019-10-01 14:57:18 -0700

    Update build.gradle to include Moshi and update PokemonDao to use Retrofit and OkHttp.

--- M02/Assignment/SprintChallenge3_PokeApi/app/src/main/java/com/lambdaschool/sprintchallenge3_pokeapi/PokemonDao.kt
+++ M02/Assignment/SprintChallenge3_PokeApi/app/src/main/java/com/lambdaschool/sprintchallenge3_pokeapi/PokemonDao.kt
@@ -1,20 +1,42 @@
 package com.lambdaschool.sprintchallenge3_pokeapi
 
-import org.json.JSONException
-import org.json.JSONObject
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import java.util.concurrent.TimeUnit
 
 object PokemonDao {
+
+    interface PokemonAPI {
+
+        @GET("pokemon/{name-id}")
+        fun getPokemon(@Path("name-id") nameOrId: String) : Call<Pokemon>
+    }
+
     private val POKEMON_URL = "https://pokeapi.co/api/v2/pokemon/%s/" // add pokemon name as
 
-    fun getPokemon(name: String): Pokemon {
-        var pokemon: Pokemon
-        try {
-            pokemon = Pokemon(JSONObject(NetworkAdapter.httpRequest(String.format(POKEMON_URL, name), "GET")))
-        } catch (e: JSONException) {
-            e.printStackTrace()
-            pokemon = Pokemon(name)
+    fun getPokemon(name: String): Call<Pokemon> {
+        val logger = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BASIC
+            level = HttpLoggingInterceptor.Level.BODY
         }
 
-        return pokemon
+        val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .retryOnConnectionFailure(false)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .connectTimeout(15, TimeUnit.SECONDS)
+                .build()
+
+        val retrofit = Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl(POKEMON_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+        return retrofit.create(PokemonAPI::class.java).getPokemon(name)
     }
 }
