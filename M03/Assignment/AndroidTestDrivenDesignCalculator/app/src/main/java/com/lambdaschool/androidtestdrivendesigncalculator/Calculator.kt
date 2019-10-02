4866de7e71d1e0dcbe4f83d14c96c31d08b79b50
msmaitran <itsmaisn@gmail.com>
2019-10-02 14:32:11 -0700

    Write two functions in the Calculator class.

--- M03/Assignment/AndroidTestDrivenDesignCalculator/app/src/main/java/com/lambdaschool/androidtestdrivendesigncalculator/Calculator.kt
+++ M03/Assignment/AndroidTestDrivenDesignCalculator/app/src/main/java/com/lambdaschool/androidtestdrivendesigncalculator/Calculator.kt
@@ -2,4 +2,14 @@ package com.lambdaschool.androidtestdrivendesigncalculator
 
 // Build a Calculator class with a constructor injected parameter of type Math (which is another class)
 class Calculator(private val math: Math) {
+
+    // The first function should be named factorial that accepts one parameter that calls the corresponding function in the Math class.
+    fun factorial(num1: Long, num2: Long): Long {
+        return math.squaresSum(num1, num2)
+    }
+
+    // The second function should be named difference that accepts two parameters that calls the corresponding function in the Math class.
+    fun difference(num1: Long, num2: Long): Long {
+        return math.absoluteDifference(num1, num2)
+    }
 }
--- M03/Assignment/AndroidTestDrivenDesignCalculator/app/src/main/java/com/lambdaschool/androidtestdrivendesigncalculator/Math.kt
+++ M03/Assignment/AndroidTestDrivenDesignCalculator/app/src/main/java/com/lambdaschool/androidtestdrivendesigncalculator/Math.kt
@@ -6,7 +6,7 @@ class Math {
 
     // Calculate the sum of squares of two numbers.
     fun squaresSum(num1: Long, num2: Long): Long {
-        return num1 * num1 + num2 * num2
+        return (num1 * num1) + (num2 * num2)
     }
 
     // Calculate the difference between the absolute values of two numbers.
