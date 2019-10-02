10d50afd42cd05dcfebb4a7a2d2ab0df3d5a6407
msmaitran <itsmaisn@gmail.com>
2019-10-02 13:38:25 -0700

    Build a Calculator class.

---
+++ M03/Assignment/AndroidTestDrivenDesignCalculator/app/src/main/java/com/lambdaschool/androidtestdrivendesigncalculator/Calculator.kt
@@ -0,0 +1,5 @@
+package com.lambdaschool.androidtestdrivendesigncalculator
+
+// Build a Calculator class with a constructor injected parameter of type Math (which is another class)
+class Calculator(private val math: Math) {
+}
